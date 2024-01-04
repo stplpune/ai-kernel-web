c084977dad7b22e8ec70627452b35c924ecfe0b2
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2024-01-04 14:32:26 +0530

    go to top button added

--- src/app/layouts/public/footer/footer.component.ts
+++ src/app/layouts/public/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  isShow: boolean = true;
+  topPosToStartShowing = 100;
 
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 }
